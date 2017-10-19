6fbcec9702f5710ffd032fbdf9f5214eaac450bd
Guy Cole <guycole@gmail.com>
2017-10-19 12:07:00 -0700

    initial commit

---
+++ bt1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bt1
+//
+//  Created by Guy Cole on 10/19/17.
+//  Copyright © 2017 Guy Cole. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
